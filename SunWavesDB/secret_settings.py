b1b9c2697fb0239fa96a1629e95f0a3a2a8b8482
ney927 <nbilling9@yahoo.com>
2021-06-05 13:09:30 -0400

    secret_settings

---
+++ SunWavesDB/secret_settings.py
@@ -0,0 +1,4 @@
+from SunWavesDB.settings import SECRET_KEY
+
+
+SECRET_KEY = 'd%x%)!))i1f6(wqoa1-7(0p-mciy@=+77d=3c#6_#lh-3vu1%5'
--- clients/models.py
+++ clients/models.py
@@ -3,6 +3,6 @@ from django.db.models.fields import CharField
 
 # Create your models here.
 class client(models.Model):
-  name = models.CharField()
-  country = models.CharField()
+  name = models.CharField(max_length=500)
+  country = models.CharField(max_length=500)
 
